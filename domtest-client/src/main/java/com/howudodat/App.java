df3be494878a2ab13c764f5de4ef8cec12500352
Peter Carlson <peter.howudodat.com>
2024-07-24 17:30:14 -0700

    demo runaway memory

--- domtest-client/src/main/java/com/howudodat/App.java
+++ domtest-client/src/main/java/com/howudodat/App.java
@@ -1,81 +1,170 @@
 package com.howudodat;
 
-import org.dominokit.domino.ui.forms.LongBox;
-import org.dominokit.domino.ui.forms.TextBox;
-import org.dominokit.domino.ui.forms.suggest.Select;
-import org.dominokit.domino.ui.forms.suggest.SelectOption;
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.dominokit.domino.ui.button.Button;
+import org.dominokit.domino.ui.datatable.ColumnConfig;
+import org.dominokit.domino.ui.datatable.DataTable;
+import org.dominokit.domino.ui.datatable.TableConfig;
+import org.dominokit.domino.ui.datatable.plugins.pagination.BodyScrollPlugin;
+import org.dominokit.domino.ui.datatable.plugins.pagination.BodyScrollPluginConfig;
+import org.dominokit.domino.ui.datatable.store.LocalListScrollingDataSource;
+import org.dominokit.domino.ui.elements.DivElement;
+import org.dominokit.domino.ui.icons.MdiIcon;
+import org.dominokit.domino.ui.icons.lib.Icons;
 import org.dominokit.domino.ui.layout.AppLayout;
-import org.dominokit.domino.ui.menu.direction.DropDirection;
+import org.dominokit.domino.ui.style.Calc;
 import org.dominokit.domino.ui.style.DominoCss;
-import org.dominokit.domino.ui.utils.DominoUIConfig;
 import org.dominokit.domino.ui.utils.ElementsFactory;
-import org.gwtproject.core.client.GWT;
-import org.gwtproject.i18n.client.DateTimeFormat;
+import org.dominokit.domino.ui.utils.PostfixAddOn;
+import org.dominokit.domino.ui.utils.Unit;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class App implements EntryPoint, ElementsFactory, DominoCss {
 	protected AppLayout layout = AppLayout.create("Domino-ui test");
-	public static DateTimeFormat sdfLongMs = DateTimeFormat.getFormat("MM/dd/yyyy HH:mm:ss.SSS");
-
+	protected LocalListScrollingDataSource<JClient> ds = new LocalListScrollingDataSource<>(50);
+	protected DataTable<JClient> tbl = null;
+	protected ArrayList<JClient> data = new ArrayList<>();
+	protected Random rand = new Random();
+		 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		TextBox tb1 = TextBox.create();
-		LongBox lb1 = LongBox.create();
 
-		tb1.getInputElement().addEventListener("input", evt -> GWT.log("TextBox Changed"));
-		lb1.getInputElement().addEventListener("input", evt -> GWT.log("LongBox Changed"));
+		createInitialData();
 
 		layout.withContent((parent1, content) -> {
-			content.appendChild(tb1);
-			content.appendChild(lb1);
+			content.appendChild(createPage());
 		});
 
-		body().appendChild(layout);
-	}
-
-	public void onModuleLoadXX() {
-		String items[] = { "", "Alice Long Last Name", "Allen With Last Name", "Bob Forgot My Last Name", "Brian No Last Name"};
-
-		DominoUIConfig.CONFIG.setClosePopupOnBlur(true);
-		Select<String> sel = Select.<String>create().addCss(dui_clearable);
-		Select<String> sel2 = Select.<String>create().addCss(dui_clearable);
-		sel.setWidth("150px");
-		sel2.setWidth("150px");
-
-		for (String s : items) sel.appendChild(SelectOption.<String>create(s, s, s));
-		for (String s : items) sel2.appendChild(SelectOption.<String>create(s, s, s));
-		sel.setSearchable(true);
-		sel2.setSearchable(true);
-		sel.getOptionsMenu().setFitToTargetWidth(false);
-		sel.getOptionsMenu().setDropDirection(DropDirection.BOTTOM_RIGHT);
-		sel2.getOptionsMenu().setDropDirection(DropDirection.BOTTOM_RIGHT);
-
-		layout.withContent((parent1, content) -> {
-			content.appendChild(sel);
-			content.appendChild(sel2);
+		layout.withNavBar((parent1, header) ->{
+			header.appendChild(PostfixAddOn.of(Button.create("rebuild").addClickListener(l->onRebuild())));
+			header.appendChild(PostfixAddOn.of(Button.create("timer").addClickListener(l-> {
+				Scheduler.get().scheduleFixedDelay(new RepeatingCommand() {
+					@Override
+					public boolean execute() {
+						onTimer();
+						return true;
+					}
+				}, 1000);
+		
+			})));
 		});
-
 		body().appendChild(layout);
+
 	}
 
-	public static native void time(String timer)
-	/*-{
-		console.time(timer);
-	}-*/;
+	protected DivElement createPage() {
+		return ElementsFactory.elements.div().appendChild(createTable());
+	}
 
-	public static native void timeEnd(String timer)
-	/*-{
-		console.timeEnd(timer);
-	}-*/;
+	protected void onRebuild() {
+		layout.getContent().clearElement()
+			.appendChild(createPage());
+	}
 
-	public static native void timeLog(String timer, String log)
-	/*-{
-		console.timeLog(timer, log);
-	}-*/;
+	protected ColumnConfig<JClient> createColumn(String col) {
+		switch (col) {
+			case "LastName":
+			return ColumnConfig.<JClient>create("LastName", "LastName")
+				.sortable()
+				.setCellRenderer(cell -> {
+					return text(cell.getRecord().LastName);
+				});	 
+			case "First1":
+			return ColumnConfig.<JClient>create("First1", "First1")
+				.sortable()
+				.setCellRenderer(cell -> {
+					return text(cell.getRecord().First1);
+				});	 
+			case "First2":
+			return ColumnConfig.<JClient>create("First2", "First2")
+				.sortable()
+				.setCellRenderer(cell -> {
+					return text(cell.getRecord().First2);
+				});	 
+			case "Age":
+			return ColumnConfig.<JClient>create("Age", "Age")
+				.sortable()
+				.setCellRenderer(cell -> {
+					return text(""+cell.getRecord().Age);
+				});	 
+			case "Weight":
+			return ColumnConfig.<JClient>create("Weight", "Weight")
+				.sortable()
+				.setCellRenderer(cell -> {
+					return text(""+cell.getRecord().Weight);
+				});	 
+
+
+			default:
+			return ColumnConfig.<JClient>create(col, col).setCellRenderer( cell -> { return text(col); });
+		}
+	}
+
+	protected DataTable<JClient> createTable() {
+		String arrChoices[] = { "LastName", "First1", "First2", "Age", "Weight" };
+
+		TableConfig<JClient> tableConfig = new TableConfig<>();
+		tableConfig.setFixed(true);
+		tableConfig.addColumn(ColumnConfig.<JClient>create("Refer", "")
+			.setCellRenderer(
+				cell -> {
+					MdiIcon icnSend = Icons.send();
+					PopPicker<JClient> pop = new PopPicker<>(icnSend, "Please Select a Client");
+					pop.addSelectionListener((pe, val) -> onRefer(val, cell.getRecord()));
+					return icnSend.element();
+		}));
+
+		for (String col : arrChoices) 
+			tableConfig.addColumn(createColumn(col));
+
+		tableConfig.setFixed(true)
+			.setFixedBodyHeight(Calc.sub(Unit.vh.of(100), Unit.px.of(110)))
+			.setMultiSelect(false)
+			.addPlugin((new BodyScrollPlugin<JClient>()).setConfig(new BodyScrollPluginConfig(25)))
+		;
+
+
+		tbl = new DataTable<>(tableConfig, ds);
+		tbl.removeCss("dui-datatable-striped");
+		ds.setData(data);
+		ds.load();
+		return tbl;
+	}
+
+	protected void createInitialData() {
+ 		for (int x=0;x<2000;x++) {
+			JClient c = new JClient();
+			c.LastName = "Last Name " + x;
+			c.First1 = "First " + x;
+			c.Age = rand.nextInt(150);
+			c.Weight = rand.nextInt(400);
+			data.add(c);
+		}
+	}
+
+	protected void onTimer() {
+		int ndx = rand.nextInt(20);
+		JClient c = data.get(ndx);
+		c.First2 = "C : " + ++c.changed;
+		c.Age = rand.nextInt(150);
+		c.Weight = rand.nextInt(400);
+
+		ds.updateRecord(c);
+		GWT.log(c.First1 + ":" + c.First2);
+	}
+
+	protected void onPreview(JClient client) {}
+	protected void onRefer(JClient c1, JClient c2) {}
+
 }
--- domtest-client/src/main/java/com/howudodat/PopPicker.java
+++ domtest-client/src/main/java/com/howudodat/PopPicker.java
@@ -1,6 +1,14 @@
 package com.howudodat;
+
+import static org.dominokit.domino.ui.style.DisplayCss.dui_elevation_0;
+import static org.dominokit.domino.ui.style.SpacingCss.dui_m_2px;
+import static org.dominokit.domino.ui.style.SpacingCss.dui_rounded_sm;
+import static org.dominokit.domino.ui.style.SpacingCss.dui_whitespace_nowrap;
+import static org.dominokit.domino.ui.utils.DomElements.dom;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.dominokit.domino.ui.IsElement;
 import org.dominokit.domino.ui.cards.Card;
@@ -8,116 +16,102 @@ import org.dominokit.domino.ui.datatable.ColumnConfig;
 import org.dominokit.domino.ui.datatable.DataTable;
 import org.dominokit.domino.ui.datatable.TableConfig;
 import org.dominokit.domino.ui.datatable.TableRow;
-import org.dominokit.domino.ui.datatable.events.SearchEvent;
-import org.dominokit.domino.ui.datatable.model.Category;
-import org.dominokit.domino.ui.datatable.model.Filter;
 import org.dominokit.domino.ui.datatable.plugins.row.RowClickPlugin;
-import org.dominokit.domino.ui.datatable.plugins.selection.SelectionPlugin;
 import org.dominokit.domino.ui.datatable.store.LocalListDataStore;
-import org.dominokit.domino.ui.datatable.store.SearchFilter;
+import org.dominokit.domino.ui.elements.DivElement;
 import org.dominokit.domino.ui.menu.direction.DropDirection;
 import org.dominokit.domino.ui.popover.Popover;
-import org.dominokit.domino.ui.utils.ElementsFactory;
-
-import com.google.gwt.core.client.GWT;
+import org.dominokit.domino.ui.utils.HasSelectionListeners.SelectionListener;
 
 import elemental2.dom.HTMLElement;
-class PopPicker<T> extends Popover {
-	protected LocalListDataStore<T> ds = new LocalListDataStore<>();
-	protected DataTable<T> tbl = null;
 
-	public PopPicker(IsElement<? extends HTMLElement> target) {
+public class PopPicker<T> extends Popover {
+	protected LocalListDataStore<T> ds = new LocalListDataStore<>(); 
+	protected DataTable<T> tbl = null;
+	protected T selectedRow = null;
+	protected SelectionListener<? super PopPicker<T>, ? super T> selectionListener = null;
+
+	public PopPicker(IsElement<? extends HTMLElement> target, String title) {
 		super(target.element());
 		this
-				.setOpenOnClick(false)
-				.closeOnEscape(true)
-				.setPosition(DropDirection.BEST_MIDDLE_DOWN_UP)
-				.appendChild(Card.create()
-						.appendChild(createChooser(this))
-						.withHeader((crd, ch) -> { ch.hide(); }))
-
-				.addExpandListener(() -> {
-					GWT.log("Expand Listener");
-					clearFilter();
-				});
+			.setOpenOnClick(true)
+			.closeOnEscape(true)
+			.setPosition(DropDirection.BEST_MIDDLE_DOWN_UP)
+			.appendChild(Card.create(title)
+				.appendChild(createChooser(this))
+				.withHeader((crd, ch) -> {
+					if (title == null || title.length() == 0)
+					ch.hide();
+				})
+				.addCss(dui_elevation_0, dui_m_2px, dui_rounded_sm)
+			);
 	}
 
+	public PopPicker<T> setData(List<T> list) {
+		ds.setData(list);
+		return this;
+	}
 	public PopPicker<T> setData(T[] list) {
 		ds.setData(Arrays.asList(list));
 		return this;
 	}
 
-	public void clearFilter() {
-		GWT.log("Clear Filter - ");
-		tbl.getSearchContext().clear();
-		// tbl.getSearchContext().fireSearchEvent();
+	public void addSelectionListener(SelectionListener<? super PopPicker<T>, ? super T> selectionListener) {
+		this.selectionListener = selectionListener;
+	}
+
+	public void setFilter(String filter) {
+	}
+
+	public T getSelectedObject() {
+		return selectedRow;
 	}
 
 	public void setSelectedObject(T val) {
 		tbl.pauseSelectionListeners();
 		List<TableRow<T>> rows = tbl.getRows();
 		for (TableRow<T> row : rows) {
-			if (row.getRecord().equals(val)) {
-				row.select();
-				break;
+			if(row.getRecord().equals(val)) { 
+				row.select(); 
+				selectedRow = row.getRecord();
+				break; 
 			}
 		}
 		tbl.resumeSelectionListeners();
 	}
 
+	protected DivElement t(String text) {
+	    return  new DivElement(dom.div()).setTextContent(text);
+	}
 	protected DataTable<T> createChooser(Popover pop) {
 		TableConfig<T> tableConfig = new TableConfig<>();
 		tableConfig
-				.setFixed(false)
-				.addColumn(ColumnConfig.<T>create("x", "x")
-						.sortable()
-						.setCellRenderer(
-								cell -> {
-									Object o = cell.getTableRow().getRecord();
-									return ElementsFactory.elements.text(o.toString());
-								}))
-				.addPlugin(new RowClickPlugin<>(row -> {
-					GWT.log("Row Click");
-					row.select();
-					pop.collapse();
-				}))
-				.setMultiSelect(false)
-				.addPlugin(new SelectionPlugin<>());
-
-		ds.setSearchFilter(new PickerSearchFilter());
+			.setFixed(false)
+			.addColumn(ColumnConfig.<T>create("x", "x")
+				.sortable()
+				.setCellRenderer(
+					cell -> {
+						Object o = cell.getTableRow().getRecord();
+							return t(o.toString()).addCss(dui_whitespace_nowrap).element();
+					}))
+			.addPlugin(new RowClickPlugin<>(row -> {
+				pop.collapse();
+				onClick(row.getRecord());
+			}))
+			.setMultiSelect(false)
+		;
+ 
 		tbl = new DataTable<>(tableConfig, ds);
 		tbl.removeCss("dui-datatable-striped");
 		tbl.headerElement().hide();
-		tbl.addSelectionListener((selectedTableRows, selectedRecords) -> {
-			GWT.log("Selection Listener");
-		});
 
 		return tbl;
 
 	}
 
-	public class PickerSearchFilter implements SearchFilter<T> {
-
-		@Override
-		public boolean filterRecord(SearchEvent event, T record) {
-			GWT.debugger();
-			List<Filter> searchFilters = event.getByCategory(Category.SEARCH);
-			if (searchFilters.isEmpty())
-				return true;
-
-			if (event.getFilters().size() == 1 && event.getFilters().get(0).getFieldName() == "*")
-				return foundByGlobalSearch(record, event.getFilters().get(0));
-			return true;
-		}
-
-		private boolean foundByGlobalSearch(T record, Filter searchFilter) {
-			String filter = searchFilter.getValues().get(0).toLowerCase();
-			if (filter.length() == 0)
-				return true;
-
-			if (record.toString().toLowerCase().contains(filter))
-				return true;
-			return false;
-		}
+	protected void onClick(T tdata) {
+		selectedRow = tdata;
+		if (selectionListener != null) selectionListener.onSelectionChanged(Optional.of(this), tdata);
 	}
+
 }
